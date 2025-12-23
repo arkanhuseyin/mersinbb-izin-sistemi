4c838710584ee19497cba5f2144b41c5c17f8590
Hüseyin Arkan <arkanhuseyin33@gmail.com>
2025-12-23 16:30:52 +0300

    Vercel refresh hatasi icin rewrite kurali eklendi

--- izin-sistemi-mobil/src/config.js
+++ izin-sistemi-mobil/src/config.js
@@ -1,4 +1,4 @@
 // 👇 BURAYA GÜNCEL TÜNEL LİNKİNİ YAPIŞTIR (Sonunda / işareti olmasın)
-export const API_URL = 'https://karry-nontubular-just.ngrok-free.dev'; 
+export const API_URL = 'https://mersinbb-izin-sistemi.onrender.com'; 
 
 // Bu dosyayı kaydettiğin an, bütün uygulama bu linki kullanmaya başlar!
